5bae76dd3810e88d9046d04338318883227835ae
braven112 <braven112@gmail.com>
2014-10-09 16:27:08 -0700

    Added buttons to Destinations and changed footer button to green

--- sections/destinations.php
+++ sections/destinations.php
@@ -9,8 +9,8 @@
         <h3>Las Vegas</h3>
         <h4 class="secondary">A few nights of decadence</h4>
       </div>
-      <div class="module-content"> Vegas, do we need to say more? America's playground for grownups is open 24/7 and waiting for you. Book it and tell your friends. <br />
-        <a href="#">Learn More &raquo;</a> </div>
+      <div class="module-content"> Vegas, do we need to say more? America's playground for grownups is open 24/7 and waiting for you. Book it and tell your friends. <br /></p>
+            <button type="button" class="btn btn-ghost-navy">Learn More</button></div>
       </div>
       <div class="col-sm-4">
         <div class="destination-newyork module-Picturecontainer"></div>
@@ -18,8 +18,9 @@
           <h3>New York</h3>
           <h4 class="secondary">Come to the center of it all</h4>
         </div>
-        <div class="module-content"> It's probably the most iconic place in America and a city that truly lives up to its reputation. New York feels like the capital of the world. <br />
-          <a href="#">Learn More &raquo;</a></div>
+        <div class="module-content"> It's probably the most iconic place in America and a city that truly lives up to its reputation. New York feels like the capital of the world. <br /></p>
+            <button type="button" class="btn btn-ghost-navy">Learn More</button>
+            </div>
         </div>
         <div class="col-sm-4">
           <div class="destination-neworleans module-Picturecontainer"></div>
@@ -27,8 +28,9 @@
             <h3>New Orleans</h3>
             <h4 class="secondary">A place like no other</h4>
           </div>
-          <div class="module-content">Perhaps the most culturally diverse city in the country, you'll remember a trip to New Orleans. History, gumbo, and music all flow together in the Big Easy. <br />
-            <a href="#">Learn More &raquo;</a></div>
+          <div class="module-content"><p>Perhaps the most culturally diverse city in the country, you'll remember a trip to New Orleans. History, gumbo, and music all flow together in the Big Easy. <br /></p>
+            <button type="button" class="btn btn-ghost-navy">Learn More</button>
+          </div>
           </div>
         </div>
       </div>
