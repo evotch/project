dceaf0abd9d5c76eabecf3737e86873f8b120f56
braven112 <braven112@gmail.com>
2014-10-24 08:05:31 -0700

    more

--- pages/honeymoon.php
+++ pages/honeymoon.php
@@ -5,6 +5,9 @@
     <div class="col-sm-8">
       <h2>Greetings Loved Ones!</h2>
       <p>Welcome to Seth 'n Jamie's Honeymoon FUNd site!  Having you in our lives is gift enough (honestly we mean that!) But if you still feel compelled to give, in lieu of gifts we are humbly asking for monetary contributions to be used for a honeymoon.</p>
+      <div class="progress">
+          <div class="progress-bar progress-bar" style="width: 55%">$1578 of $6000</div>
+          </div>
           <div class="col-sm-3">
           <div class="pull-left" style="margin-right:30px;">
             <h2>30</h2>
@@ -13,7 +16,7 @@
           </div>
           <div class="col-sm-3">
           <div class="pull-left" style="margin-right:30px;">
-            <h2>$1500</h2>
+            <h2>$1578</h2>
             <p>money raised</p>
           </div>
           </div>
@@ -24,12 +27,16 @@
           </div>
           </div>
           <div class="col-sm-3" style="margin-right:30px;">
+          
+      
+          
             <p style="margin-top:10px;"><button type="button" class="btn btn-warning btn-block">Pledge</button>    </p>
           </div>
 
     </div>
     <div class="col-sm-4">
       <img class="img-rounded" height="250" src="../img/honeymoon.jpg">
+      <a href="#">Share</a>
     </div>
   </div>
 </div>
