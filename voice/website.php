357b0312cadd2b03a967b8beac41a284a0ab3839
braven112 <braven112@yahoo.com>
2014-11-02 22:20:50 -0800

    fixed callout color

--- voice/website.php
+++ voice/website.php
@@ -1,6 +1,6 @@
 <h4 id="website">Website</h4>
 <p>Website is given an initial cap only when it begins a sentence. Otherwise, write it as one word in all lower case.</p>
-<div class="callout callout-pink">
+<div class="callout callout-green">
 <h4>Correct</h4>
 	<p>Other websites can’t hold a candle to alaskaair.com. Websites, don’t even try.</p>
 </div>
