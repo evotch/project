5ddcbd9177a90ab96e5cfa91fa7a9e763dcd2d5f
braven112 <braven112@gmail.com>
2014-11-25 10:06:35 -0800

    removed underline on a.btn

--- carousels/colorproportions-carousel.php
+++ carousels/colorproportions-carousel.php
@@ -6,85 +6,132 @@
   <div class="row">
     <!-- Carousel
     ================================================== -->
-    <div data-interval="false" id="callout-carousel" class="callout-carousel carousel slide carousel-nav">
+    <div data-interval="false" id="colorproportions-carousel" class="callout-carousel carousel slide carousel-nav">
       <!-- Indicators -->
 
       <div class="carousel-inner">
         <div class="item active">
-          <div class="callout callout-navy">
-            <h4>callout-navy</h4>
-            <p>There will be times when you wish to call out certain important sections of text.  With the <code>.callout</code> you can add a border around a block of text.  There are 10 types of callouts <code>.callout-navy</code> <code>.callout-teal</code> <code>.callout-green</code> <code>.callout-brown</code> <code>.callout-orange</code> <code>.callout-gold</code> <code>.callout-plum</code> <code>.callout-pink</code> <code>.callout-blue</code></p>
-          </div>
-        </div>
-        <div class="item">
-          <div class="callout callout-teal">
-            <h4>callout-teal</h4>
-            <p>There will be times when you wish to call out certain important sections of text.  With the <code>.callout</code> you can add a border around a block of text.  There are 10 types of callouts <code>.callout-navy</code> <code>.callout-teal</code> <code>.callout-green</code> <code>.callout-brown</code> <code>.callout-orange</code> <code>.callout-gold</code> <code>.callout-plum</code> <code>.callout-pink</code> <code>.callout-blue</code></p>
-          </div>
-          
-        </div>
-        <div class="item">
-          <div class="callout callout-green">
-            <h4>callout-green, callout-success</h4>
-            <p>There will be times when you wish to call out certain important sections of text.  With the <code>.callout</code> you can add a border around a block of text.  There are 10 types of callouts <code>.callout-navy</code> <code>.callout-teal</code> <code>.callout-green</code> <code>.callout-brown</code> <code>.callout-orange</code> <code>.callout-gold</code> <code>.callout-plum</code> <code>.callout-pink</code> <code>.callout-blue</code></p>
-          </div>
-          
-        </div>
-        <div class="item">
-          <div class="callout callout-brown">
-            <h4>callout-brown</h4>
-            <p>There will be times when you wish to call out certain important sections of text.  With the <code>.callout</code> you can add a border around a block of text.  There are 10 types of callouts <code>.callout-navy</code> <code>.callout-teal</code> <code>.callout-green</code> <code>.callout-brown</code> <code>.callout-orange</code> <code>.callout-gold</code> <code>.callout-plum</code> <code>.callout-pink</code> <code>.callout-blue</code></p>
-          </div>
-          
-        </div>
-        <div class="item">
-          <div class="callout callout-orange">
-            <h4>callout-orange</h4>
-            <p>There will be times when you wish to call out certain important sections of text.  With the <code>.callout</code> you can add a border around a block of text.  There are 10 types of callouts <code>.callout-navy</code> <code>.callout-teal</code> <code>.callout-green</code> <code>.callout-brown</code> <code>.callout-orange</code> <code>.callout-gold</code> <code>.callout-plum</code> <code>.callout-pink</code> <code>.callout-blue</code></p>
+          <h3 class="page-header secondary">Accent <small>(backgrounds)</small></h3>
+          <p>When a white background doesn't stand out enough the following colors can be used as background colors.
+          </p>
+          <div class="progress">
+            <div class="progress-bar progress-bar-pink" style="width: 2%">
+              <span class="sr-only"></span>
+            </div>
+            <div class="progress-bar progress-bar-white" style="width: .5%">
+              <span class="sr-only"></span>
+            </div>
+            <div class="progress-bar progress-bar-plum" style="width: 2%">
+              <span class="sr-only"></span>
+            </div>
+            <div class="progress-bar progress-bar-white" style="width: .5%">
+              <span class="sr-only"></span>
+            </div>
+            <div class="progress-bar progress-bar-gold" style="width: 2%">
+              <span class="sr-only"></span>
+            </div>
+            <div class="progress-bar progress-bar-white" style="width: .5%">
+              <span class="sr-only"></span>
+            </div>
+            <div class="progress-bar progress-bar-orange" style="width: 2%">
+              <span class="sr-only"></span>
+            </div>
+            <div class="progress-bar progress-bar-white" style="width: 90.5%">
+              <span class="sr-only"></span>
+            </div>
           </div>
           
         </div>
         <div class="item">
-          <div class="callout callout-gold">
-            <h4>callout-gold</h4>
-            <p>There will be times when you wish to call out certain important sections of text.  With the <code>.callout</code> you can add a border around a block of text.  There are 10 types of callouts <code>.callout-navy</code> <code>.callout-teal</code> <code>.callout-green</code> <code>.callout-brown</code> <code>.callout-orange</code> <code>.callout-gold</code> <code>.callout-plum</code> <code>.callout-pink</code> <code>.callout-blue</code></p>
+          <h3 class="page-header secondary">Supporting <small>(backgrounds)</small></h3>
+          <p>When a white background doesn't stand out enough the following colors can be used as background colors.
+          </p>
+          <div class="progress">
+            <div class="progress-bar progress-bar-white" style="width: 12.5%">
+            </div>
+            <div class="progress-bar progress-bar-aqua" style="width: 6%">
+              <span class="sr-only">6% Complete (danger)</span>
+            </div>
+            <div class="progress-bar progress-bar-white" style="width: .5%">
+              <span class="sr-only"></span>
+            </div>
+            <div class="progress-bar progress-bar-cream" style="width: 14%">
+              <span class="sr-only"></span>
+            </div>
+            <div class="progress-bar progress-bar-white" style="width: .5%">
+              <span class="sr-only"></span>
+            </div>
+            <div class="progress-bar progress-bar-tan" style="width: 8%">
+              <span class="sr-only"></span>
+            </div>
+            <div class="progress-bar progress-bar-white" style="width: .5%">
+              <span class="sr-only"></span>
+            </div>
+            <div class="progress-bar progress-bar-gray-light" style="width: 3%">
+              <span class="sr-only"></span>
+            </div>
+            <div class="progress-bar progress-bar-white" style="width: 55%">
+              <span class="sr-only"></span>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <h3 class="page-header secondary">Subordinate <small>(reinforece the message)</small></h3>
+          <p>Use blue and brown to reinforce the message as needed.
+          </p>
+          <div class="progress">
+            <div class="progress-bar progress-bar-white" style="width: 48%">
+              <span class="sr-only"></span>
+            </div>
+            <div class="progress-bar progress-bar-blue" style="width: 4%">
+              <span class="sr-only"></span>
+            </div>
+            <div class="progress-bar progress-bar-white" style="width: .5%">
+              <span class="sr-only"></span>
+            </div>
+            <div class="progress-bar progress-bar-brown" style="width: 4%">
+              <span class="sr-only">35% Complete (success)</span>
+            </div>
+            <div class="progress-bar progress-bar-white" style="width: 43.5%">
+              <span class="sr-only"></span>
+            </div>
           </div>
           
         </div>
         <div class="item">
-          <div class="callout callout-plum">
-            <h4>callout-plum</h4>
-            <p>There will be times when you wish to call out certain important sections of text.  With the <code>.callout</code> you can add a border around a block of text.  There are 10 types of callouts <code>.callout-navy</code> <code>.callout-teal</code> <code>.callout-green</code> <code>.callout-brown</code> <code>.callout-orange</code> <code>.callout-gold</code> <code>.callout-plum</code> <code>.callout-pink</code> <code>.callout-blue</code></p>
+          <h3 class="page-header secondary">Dominant <small>(convey the message)</small></h3>
+          <p>Green, Marine (teal), Navy are our dominant colors.
+          </p>
+          <div class="progress">
+            <div class="progress-bar progress-bar-white" style="width: 57%">
+              <span class="sr-only"></span>
+            </div>
+            <div class="progress-bar progress-bar-green" style="width: 12%">
+              <span class="sr-only">35% Complete (success)</span>
+            </div>
+            <div class="progress-bar progress-bar-white" style="width: .5%">
+              <span class="sr-only"></span>
+            </div>
+            <div class="progress-bar progress-bar-teal" style="width: 12%">
+              <span class="sr-only">20% Complete (warning)</span>
+            </div>
+            <div class="progress-bar progress-bar-white" style="width: .5%">
+              <span class="sr-only"></span>
+            </div>
+            <div class="progress-bar progress-bar-navy" style="width: 18%">
+              <span class="sr-only"></span>
+            </div>
           </div>
           
         </div>
-        <div class="item">
-          <div class="callout callout-pink">
-            <h4>callout-pink</h4>
-            <p>There will be times when you wish to call out certain important sections of text.  With the <code>.callout</code> you can add a border around a block of text.  There are 10 types of callouts <code>.callout-navy</code> <code>.callout-teal</code> <code>.callout-green</code> <code>.callout-brown</code> <code>.callout-orange</code> <code>.callout-gold</code> <code>.callout-plum</code> <code>.callout-pink</code> <code>.callout-blue</code></p>
-          </div>
-          
-        </div>
-        <div class="item">
-          <div class="callout callout-blue">
-            <h4>callout-blue</h4>
-            <p>There will be times when you wish to call out certain important sections of text.  With the <code>.callout</code> you can add a border around a block of text.  There are 10 types of callouts <code>.callout-navy</code> <code>.callout-teal</code> <code>.callout-green</code> <code>.callout-brown</code> <code>.callout-orange</code> <code>.callout-gold</code> <code>.callout-plum</code> <code>.callout-pink</code> <code>.callout-blue</code></p>
-          </div>
-          
-        </div>
-        <div class="item">
-          <div class="callout callout-aqua">
-            <h4>callout-aqua, callout-info</h4>
-            <p>There will be times when you wish to call out certain important sections of text.  With the <code>.callout</code> you can add a border around a block of text.  There are 10 types of callouts <code>.callout-navy</code> <code>.callout-teal</code> <code>.callout-green</code> <code>.callout-brown</code> <code>.callout-orange</code> <code>.callout-gold</code> <code>.callout-plum</code> <code>.callout-pink</code> <code>.callout-blue</code></p>
-          </div>
-          
-        </div>
+
+
       </div>
       <!-- Controls -->
-      <a class="left carousel-control" href="#callout-carousel" data-slide="prev">
+      <!--<a class="left carousel-control" href="#callout-carousel" data-slide="prev">
         <span class="icon-prev"></span>
-      </a>
-      <a class="right carousel-control" href="#callout-carousel" data-slide="next">
+      </a>-->
+      <a class="right carousel-control" style="margin-top:-80px;" href="#colorproportions-carousel" data-slide="next">
         <span class="icon-next"></span>
       </a>
     </div>
