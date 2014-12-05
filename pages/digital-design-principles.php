62e7eb8d42b1b2f34caaab85a692da7a814da8de
braven112 <braven112@yahoo.com>
2014-12-05 14:23:40 -0800

    update principles

--- pages/digital-design-principles.php
+++ pages/digital-design-principles.php
@@ -10,7 +10,7 @@
 
     <ul>
     <li>Our designs enable Alaska’s business objectives and support demonstrable business value</li>
-    <li>Our designs are customer-centered and data-driven</li>
+    <li>Our designs are customer-centered and data-influenced</li>
     <li>Our design work is on-brand and consistent across channels</li>
     <li>Our designs support customers’ journey with Alaska and are sensitive to where customers are on that journey</li>
     <li>Our designs are simple and intuitive; we err on the side of less versus more</li>
@@ -18,6 +18,8 @@
     <li>Our designs are based on a common design language supported by the Digital Style Guide</li>
 
     </ul>
+
+    <br><br><br><br><br><br><br><br>
 </div>
 <div class="col-md-4">
 </div>
