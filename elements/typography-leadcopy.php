1adad60f3cd97af5e09693bc2370976bf3dfe79f
braven112 <braven112@yahoo.com>
2014-12-05 16:52:27 -0800

    lead update

--- elements/typography-leadcopy.php
+++ elements/typography-leadcopy.php
@@ -2,23 +2,23 @@
 <div role="tabpanel">
   <!-- Nav tabs -->
   <ul class="nav nav-tabs dsg" role="tablist">
-    <li role="presentation" class="active"><a href="#Lead-CodeExample" aria-controls="Lead-CodeExample" role="tab" data-toggle="tab">Code Example</a></li>
-    <li role="presentation"><a href="#Lead-VoiceGuidlines" aria-controls="Lead-VoiceGuidlines" role="tab" data-toggle="tab">Voice Guidlines</a></li>
-    <li role="presentation"><a href="#Lead-SEO" aria-controls="Lead-SEO" role="tab" data-toggle="tab">SEO</a></li>
-    <li role="presentation"><a href="#Lead-Mobile" aria-controls="Lead-Mobile" role="tab" data-toggle="tab">Mobile</a></li>
-    <li role="presentation"><a href="#Lead-Email" aria-controls="Lead-Email" role="tab" data-toggle="tab">Email</a></li>    
+    <li role="presentation" class="active"><a href="#lead-CodeExample" aria-controls="lead-CodeExample" role="tab" data-toggle="tab">Code Example</a></li>
+    <li role="presentation"><a href="#lead-VoiceGuidlines" aria-controls="lead-VoiceGuidlines" role="tab" data-toggle="tab">Voice Guidlines</a></li>
+    <li role="presentation"><a href="#lead-SEO" aria-controls="lead-SEO" role="tab" data-toggle="tab">SEO</a></li>
+    <li role="presentation"><a href="#lead-Mobile" aria-controls="lead-Mobile" role="tab" data-toggle="tab">Mobile</a></li>
+    <li role="presentation"><a href="#lead-Email" aria-controls="lead-Email" role="tab" data-toggle="tab">Email</a></li>    
   </ul>
   <!-- Tab panes -->
   <div class="tab-content">
-    <div role="tabpanel" class="tab-pane active" id="Lead-CodeExample">
+    <div role="tabpanel" class="tab-pane active" id="lead-CodeExample">
      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel enim vulputate, laoreet libero condimentum, blandit felis. </p>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel enim vulputate, laoreet libero condimentum, blandit felis. Nunc tortor erat, sodales sit amet euismod a, facilisis ut ligula. Vestibulum sit amet ipsum bibendum, fringilla ligula sit amet, efficitur orci. Ut turpis magna, consequat in sem et, pharetra auctor lorem. Nulla sollicitudin sapien orci, ac egestas odio gravida sit amet. Curabitur sagittis est tincidunt, egestas enim sed, maximus lorem. Aliquam lacus dui, congue eu ligula non, placerat tristique dui. Nulla hendrerit nulla sed sollicitudin feugiat. Nullam ut dui vel mauris maximus facilisis ut et sapien.</p>
             <pre><code class="html">&lt;p class="lead"&gt;...&lt;/p&gt;</code></pre>
-</div>
-    <div role="tabpanel" class="tab-pane" id="buttons-VoiceGuidlines"><div class="item">Coming Soon!</div></div>
-    <div role="tabpanel" class="tab-pane" id="buttons-SEO"><div class="item">Coming Soon!</div></div>
-    <div role="tabpanel" class="tab-pane" id="buttons-Mobile"><div class="item">Coming Soon!</div></div>
-    <div role="tabpanel" class="tab-pane" id="buttons-Email"><div class="item">Coming Soon!</div></div>
-    <div role="tabpanel" class="tab-pane" id="buttons-Kiosk"><div class="item">Coming Soon!</div></div>    
+    </div>
+    <div role="tabpanel" class="tab-pane" id="lead-VoiceGuidlines"><div class="item">Coming Soon!</div></div>
+    <div role="tabpanel" class="tab-pane" id="lead-SEO"><div class="item">Coming Soon!</div></div>
+    <div role="tabpanel" class="tab-pane" id="lead-Mobile"><div class="item">Coming Soon!</div></div>
+    <div role="tabpanel" class="tab-pane" id="lead-Email"><div class="item">Coming Soon!</div></div>
+    <div role="tabpanel" class="tab-pane" id="lead-Kiosk"><div class="item">Coming Soon!</div></div>    
   </div>
 </div>
