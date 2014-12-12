9a5fded0f7e4e93571bfa8121d53f072fdc0c780
braven112 <braven112@gmail.com>
2014-12-11 22:43:25 -0800

    beyond

---
+++ pages/beyond-media.php
@@ -0,0 +1,371 @@
+<?php include ("../sections/header-brand.php"); ?>
+<link href='http://www.alaskaair.com/content/~/media/Files/fonts/atlas/atlas' rel='stylesheet' type='text/css'>
+<style type="text/css">
+
+
+#navbar {
+  /*width: 100%;*/
+  position:static;
+  top:-32px;
+  margin-bottom: 3em;
+  background-color: #f4f4f4 !important;
+  color: #666 !important;
+  border: 2px solid #ccc !important;
+  box-shadow: 1px 1px 3px 0 rgba(0,0,0,.3)
+}
+
+#navbar.affix {
+   position: fixed;
+   top: 0;
+   z-index:10;
+   -webkit-transition: all .6s ease-in-out;
+}
+
+.col-lg-12 img.moviePoster {
+  float:left;
+}
+header {
+  background-image:url(/~/media/EBE34DC996974282AC9BF863BFA767AC.ashx);
+  height:350px;
+  color:#fff;
+}
+
+@media (min-width: 767px) {
+  .navbar-nav.nav-justified > li{
+      float:none;
+  }
+}
+.navbar-nav {
+  margin: 1px 1px; 
+}
+.scroll-top {
+   position:fixed;
+   bottom:0;
+   right:6%;
+   z-index:100;
+   background: #ffcc33;
+   font-size:24px;
+   border-top-left-radius:3px;
+   border-top-right-radius:3px;
+}
+.scroll-top a:link,.scroll-top a:visited {
+  color:#222;
+} 
+.hero-bg{
+height: 268px;
+background-image:url(/~/media/EBE34DC996974282AC9BF863BFA767AC.ashx);
+}
+ 
+h3 {
+color: #00245A !important;
+}
+
+
+
+</style>
+
+<div class="container">
+<div id="navbar" class="navbar-collapse collapse affix-top" data-spy="affix" data-offset-top="60" data-offset-bottom="200">
+  <ul class="nav navbar-nav nav-justified text-uppercase text-center">
+    <li class="active"><a href="#wifi-inflight">Inflight Wifi</a></li>
+    <li>|</li>
+    <li><a href="#fun-inflight">Inflight Entertainment</a></li>
+    <li>|</li>
+    <li><a href="aa-content">Alaska Content</a></li>
+    <li>|</li>
+    <li><a href="#mag-inflight">Inflight Magazine</a></li>
+  </ul>
+</div><!--/.nav-collapse -->
+
+<div class="row">
+  <h3 class="text-info text-center">Intro Title</h3>
+  <div class="col-sm-12"></div>
+  <div class="col-sm-12 hero-bg">
+
+  </div>
+  <div class="col-sm-12">
+    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.</p>
+    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>    </div><br>
+  </div>
+  <div class="row">
+    <hr>
+    <h3 id="wifi-inflight" class="text-info text-center">Inflight Wifi</h3>
+    <div class="col-sm-12">
+      <div id="sect2" class="col-sm-12">
+      </div>
+      <div class="col-sm-12">
+        <p>We've commissioned custom-designed <strong>Recaro</strong> leather seats, designed to offer you more legroom without compromising comfort or recline.</p>
+        <p>Now in every one of those seats, on every Alaska flight, you can stay fully charged with 110 volt power and USB outlets in the seatback. There's no need to put your life on hold just because you're at 35,000 feet, stay connected with power and Wi-Fi on every flight.</p>
+      </div>
+      <div class="col-sm-12">
+        <h3 class="text-info text-center">Fly with the Internet using Gogo&reg;</h3>
+        <p>Alaska Airlines is pleased to offer Gogo Inflight Internet on almost all of our aircraft operating within the Lower 48 United States and specific areas in Alaska.</p>
+        <p>Use your laptop or other Wi-Fi enabled mobile device to surf the web, instant message, email and VPN in the air. Look for the Wi-Fi symbol to the right as you board your aircraft to see if you will have Wi-Fi on your flight.Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo</p>
+        <div class="col-sm-6">
+          <div class="well well-sm">
+            <ol>
+              <li>Turn on your Wi-Fi</li>
+              <li>Find the gogoinflight signal and connect</li>
+              <li>Launch your web browser and sign up</li>
+            </ol>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <h3>Gogo Pricing</h3>
+          <p>Gogo has flexible pricing...Morbi in sem quis dui placerat ornare. Pellentesque odio nisi diam. Sed arcu. </p>
+        </div>
+      </div>
+      <div class="col-sm-12">
+        <h3 class="gogo-opt">Gogo Inflight Internet FAQ | Review day an monthly pass options.</h3>
+        <h3>Gogo Inflight Internet Coverage</h3>
+        <div style="height:700px;" class="col-sm-12 hero-bg">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <hr>
+    <h3 id="fun-inflight" class="text-info text-center">Inflight Entertainment</h3>
+    <div class="col-sm-12 hero-bg"></div>
+    
+    
+    <div class="col-sm-12">
+      <h3>Movies, Music and More</h3>
+      <p>Enjoy movies, music and more with our inflight entertainment devices.Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. </p>
+    </div>
+    <div class="col-lg-12">
+      <p>Enjoy movies, music and more with our inflight entertainment devices. This handheld marvel has the ability to keep you entertained the whole flight with over 50 hit movies, plus sports, great TV, music, destination information, discounted Gogo Internet access and more. <a href="https://www.digeplayer.com/reserve/">Reserve</a> on select routes at least 24 hours in advance.</p>
+      
+      <h2 class="sc blockbuster">Blockbuster Hits This Month</h2>
+      <div class="row">
+        <div class="col-lg-8">
+          <img class="moviePoster squareShadow" src="//resource.alaskaair.net/~/media/Images/photos-infographics/technology/inflight-entertainment/theMazeRunner.ashx" title="The Maze Runner" alt="The Maze Runner">
+          <div class="col-lg-5">
+            <p><strong>The Maze Runner</strong><br>(PG13, 1:53)</p>
+            <p>Thomas is deposited in a community of boys after his memory is erased, soon learning they're all trapped in a maze that will require him to join forces with fellow "runners" for a shot at escape.</p>
+          </div>
+        </div>
+        
+        <div class="col-lg-8">
+          <img class="moviePoster squareShadow" src="//resource.alaskaair.net/~/media/Images/photos-infographics/technology/inflight-entertainment/dolphinTale2.ashx" title="Dolphin Tale 2" alt="Dolphin Tale 2">
+          <div class="col-lg-5">
+            <p><strong>Dolphin Tale 2</strong><br>(PG, 1:47)</p>
+            <p>The team of people who saved Winter's life reassemble in the wake of her surrogate mother's passing in order to find her a companion so she can remain at the Clearwater Marine Hospital.  <em>Available in English subtitles.</em></p>
+          </div>
+        </div>
+        
+        <div class="col-lg-8"> <img class="moviePoster squareShadow" src="//resource.alaskaair.net/~/media/Images/photos-infographics/technology/inflight-entertainment/ifIStay.ashx" title="If I Stay" alt="If I Stay">
+          <div class="col-lg-5">        <p><strong>If I Stay</strong><br>(PG13, 1:47)</p>
+            <p>Life changes in an instant for young Mia Hall after a car accident puts her in a coma. During an out-of-body experience, she must decide whether to wake up and live a life far different than she had imagined.  <em>Available in English subtitles.</em></p>
+          </div>
+          <div class="clear"> </div>
+        </div>
+        
+        <div class="col-lg-8"> <img class="moviePoster squareShadow" src="//resource.alaskaair.net/~/media/Images/photos-infographics/technology/inflight-entertainment/theHundredFootJourney.ashx" title="The Hundred Foot Journey" alt="The Hundred Foot Journey">
+          <div class="col-lg-5">        <p><strong>The Hundred Foot Journey</strong><br>(PG, 2:02)</p>
+            <p>The Kadam family leaves India for France where they open a restaurant directly across the road from Madame Mallory's Michelin-starred eatery.   <em>Available in English subtitles.</em></p>
+          </div>
+          <div class="clear"> </div>
+        </div>
+        
+        <div class="col-lg-8"> <img class="moviePoster squareShadow" src="//resource.alaskaair.net/~/media/Images/photos-infographics/technology/inflight-entertainment/intoTheStorm.ashx" title="Into The Storm" alt="Into The Storm">
+          <div class="col-lg-5">        <p><strong>Into The Storm</strong><br>(PG13, 1:28)</p>
+            <p>In the span of a single day, the town of Silverton is ravaged by an unprecedented onslaught of tornadoes. Most people seek shelter, while others run towards the vortex, testing how far a storm chaser will go for that once-in-a-lifetime shot. Told through the eyes and lenses of professional storm chasers, thrill-seeking amateurs, and courageous townspeople.</p>
+          </div>
+          <div class="clear"> </div>
+        </div>
+        
+        <div class="col-lg-8"> <img class="moviePoster squareShadow" src="//resource.alaskaair.net/~/media/Images/photos-infographics/technology/inflight-entertainment/jerseyBoys.ashx" title="Jersey Boys" alt="Jersey Boys">
+          <div class="col-lg-5">        <p><strong>Jersey Boys</strong><br>(R <em>edited</em>, 2:10)</p>
+            <p>Clint Eastwood's big screen version of the Tony Award-winning musical tells the story of the four young men from the wrong side of the tracks in New Jersey who formed the iconic `60s rock group The Four Seasons. Their trials and triumphs are accompanied by the hit songs that influenced a generation, and are now being embraced by a new generation of fans through the stage musical.</p>
+          </div>
+          <div class="clear"> </div>
+        </div>
+        
+        <br />
+        <div class="col-lg-8">
+          <h2 class="sc popClassics">Popular Classics</h2>
+          <div>Enjoy over 50 popular selections from the following categories:</div>
+          <div class="col-lg-5">
+            <ul>
+              <li>Adventure</li>
+              <li>Drama</li>
+              <li>Comedy</li>
+            </ul>
+          </div>
+          <div class="col-lg-5">
+            <ul>
+              <li>Romance</li>
+              <li>Oscar Winners</li>
+            </ul>
+          </div>
+          <div class="clear"> </div>
+          
+          <div class="col-lg-5">
+            <h2 class="sc">TV Shows</h2>
+            <ul>
+              <li>Mom</li>
+              <li>Person of Interest</li>
+              <li>The Big Bang Theory</li>
+              <li>New Girl</li>
+              <li>Bones</li>
+              <li>Bob's Burgers</li>
+              <li>Dads</li>
+            </ul>
+          </div>
+          <div class="col-lg-5">    <ul>
+            <li>Raising Hope</li>
+            <li>The Middle</li>
+            <li>2 Broke Girls</li>
+            <li>Hollywood Scandals</li>
+            <li>ReelzChannel Special</li>
+            <li>Beverly Hills Pawn</li>
+          </ul>
+        </div>
+        <div class="clear"> </div>
+        
+        <div class="col-lg-5">  <h2 class="sc">Discover Alaska</h2>
+          <ul>
+            <li>Alaska Airlines Air-to-Air</li>
+            <li>Alaska Mint</li>
+            <li>David Green Master Furrier</li>
+          </ul>
+          <ul>
+            <li>Ulu Factory</li>
+            <li>Oomingmak Musk Ox Producers Co-Op</li>
+          </ul>
+        </div>
+        <div class="clear"> </div>
+        <div class="col-lg-5">  <h2 class="sc">Discover Hawaii</h2>
+          <ul>
+            <li>Oahu Up Close</li>
+            <li>Kauai Up Close</li>
+          </ul>
+          <ul>
+            <li>Maui Up Close</li>
+            <li>Visions of Hawaii</li>
+          </ul>
+        </div>
+        
+        <div class="col-lg-5">
+          <h2 class="sc">Disney Kids</h2>
+          <p>  Great TV and movies just for kids</p>
+          <div class="clear"> </div>
+          <h2 class="sc">Music</h2>
+          <p>Over 200 songs from various genres</p>
+          <div class="clear"> </div>
+        </div>
+        
+        
+        <h3>Pricing &amp; Availability</h3>
+        <p>Free for First Class travelers, <br>no reservation required</p>
+        
+        <h4>Non-Reservable Flights $8</h4>
+        <table class="default" style="WIDTH: 190px">
+          <tbody>
+            <tr>
+              <th style="width:85px;">Between</th>
+              <th style="width:105px;">And</th></tr>
+              <tr>
+                <td style="vertical-align:top;">Portland</td>
+                <td>Anchorage, Fairbanks</td></tr>
+                <tr>
+                  <td style="vertical-align:top;">Seattle</td>
+                  <td>Anchorage, Fairbanks</td></tr>
+                </tbody>
+              </table><br>
+              
+              <h4>Reservable Flights $8</h4>
+              <table class="default" style="width:190px;">
+                <tbody>
+                  <tr>
+                    <th style="width:85px;">Between</th>
+                    <th style="width:105px;">And</th>
+                  </tr>
+                  <tr>
+                      <td style="vertical-align:top;">Anchorage</td>
+                      <td>Las Vegas, Los Angeles, Phoenix</td></tr>
+                      <tr>
+                        <td style="vertical-align:top;">Portland</td>
+                        <td>Chicago, Dallas/Ft.Worth, <nobr>Los Cabos,</nobr> <nobr>Puerto Vallarta,</nobr></td></tr>
+                        <tr>
+                          <td style="vertical-align:top;">Seattle</td>
+                          <td>Austin, Chicago, Dallas/Ft. Worth, Houston, Kansas City, Los Cabos, Minneapolis, Puerto Vallarta, St. Louis, San Antonio</td></tr>
+                        </tbody>
+                      </table>
+                      <br>
+                      
+                      <h4>Reservable Flights $10</h4>
+                      
+                      <table class="default" style="width:190px;">
+                        <tbody>
+                          <tr>
+                            <th style="width:85px;">Between</th>
+                            <th style="width:105px;">And</th></tr>
+                            <tr>
+                              <td style="vertical-align:top;">Anchorage</td>
+                              <td>Chicago, Honolulu, Kona, Maui</td>
+                            </tr>
+                            <tr>
+                              <td><nobr>Los Angeles</nobr></td>
+                              <td>Washington D.C.</td>
+                            </tr>
+                            <tr>
+                              <td>Honolulu</td>
+                              <td>Bellingham, Oakland, Portland, San Diego, San Jose, Seattle</td>
+                            </tr>
+                            <tr>
+                              <td>Kona</td>
+                              <td>Anchorage, Oakland, Portland, San Jose, Seattle</td>
+                            </tr>
+                            <tr>
+                              <td>Lihue</td>
+                              <td>Oakland, Portland, San Diego, San Jose, Seattle</td>
+                            </tr>
+                            <tr>
+                              <td>Maui</td>
+                              <td>Bellingham, Oakland, Portland, Sacramento, San Diego, San Jose, Seattle</td>
+                            </tr>
+                            <tr>
+                              <td>Portland</td>
+                              <td>Boston, Los Cabos, <nobr>Puerto Vallarta</nobr>, Washington D.C.</td>
+                            </tr>
+                            <tr>
+                              <td>San Diego</td>
+                              <td>Orlando, Boston</td>
+                            </tr>
+                            <tr>
+                              <td style="vertical-align:top;">Seattle</td>
+                              <td>Atlanta, Baltimore, Boston, Cancun, Detroit, <nobr>Fort Lauderdale</nobr>, <nobr>New Orleans</nobr>, Newark, Orlando, Philadelphia, Tampa, Washington D.C.</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                                      </div>
+                                    </div>
+                                    </div>
+                                    <div class="col-sm-12">
+                                      <h3>How to Rent</h3>
+                                      <p>Reserve your inflight entertainment device up to 24 hours in advance. Registration is required and you must know your eligible flight number to reserve. Be sure to print out your receipt and bring it with you on your flight.</p>
+                                      
+                                      <p>You can also rent your inflight entertainment device onboard the aircraft on the day of your flight. Availability is limited, so it's best to reserve online.</p>
+                                      
+                                      <p>Onboard purchases can be made with any major credit or debit card. To accommodate Alaska residents who do not currently have a debit or credit card, vouchers can be purchased at all of our ticket counters in the state of Alaska.</p>
+                                      <button class="btn btn-lg btn-primary">Reserve Now</button>
+                                    </div>
+                                    <div class="row">
+                                      <hr>
+                                      <div class="col-sm-12">
+                                        <h3 class="text-info text-center">Alaska Content (FPO)</h3>
+                                        <div class="col-sm-10">
+                                          <img src="/~/media/8A7B5657B2164929811F72F7471A53DD.ashx" />
+                                        </div>
+                                      </div>
+                                    </div>
+<script type="text/javascript">/* affix the navbar after scroll below header */
+$('#navbar').affix({
+      offset: {
+        top: $('header').height()-$('#navbar').height()
+      }
+});
+</script>
+                                      <?php include ("../sections/footer.php"); ?>
--- pages/beyond.php
+++ pages/beyond.php
@@ -1,11 +1,31 @@
 <?php include ("../sections/header-brand.php"); ?>
-<!--<style type="text/css">.beyond-campaign .container {width: 100%;}</style>-->
+<link href='~/media/224D340AEC064E9580054EF9447DCF1D.css' rel='stylesheet' type='text/css'>
+<link href='~/media/C73783F3C0D3426A8E1B95F795439EF5.css' rel='stylesheet' type='text/css'>
+<link href='http://localhost/css/beyond.css' rel='stylesheet' type='text/css'>
 
-<div class="contentMain beyond-campaign">
+
+
+
+
+
+<div class="contentWrapNoSubNav beyond-campaign">
 <div class="container">
   <div class="row">
   <div class="col-sm-12">
-    <img src="http://placehold.it/940x350">
+  <div class="image-container"><img id="beyondMainImage" class="full-screen" src="http://placehold.it/972x350"></div>
+<div id="nav" class="navbar-collapse collapse affix" data-spy="affix" data-offset-top="60" data-offset-bottom="200">
+  <ul class="nav navbar-nav nav-justified text-uppercase text-center">
+    <li class="active"><a href="#wifi-inflight">Inflight Wifi</a></li>
+
+    <li><a href="#fun-inflight">Inflight Entertainment</a></li>
+
+    <li><a href="#aa-content">Alaska Content</a></li>
+
+    <li><a href="#mag-inflight">Inflight Magazine</a></li>
+    <!--<li id="book-link"><a class="text-success" data-toggle="collapse" data-target="#book-now"><span class="glyphicon glyphicon-plane"></span>&nbsp;<strong>Book Now</strong></a></li>-->
+  </ul> 
+</div>  
+
   </div>
   </div>
 </div>
@@ -21,19 +41,22 @@
   <div class="row">
     <div class="col-sm-12">
       <h3>Inflight Media & Wi-Fi</h3>
-        <img src="http://placehold.it/940x350">
+        <img class="full-screen" src="http://placehold.it/972x350">
       
       <p>We're curating an inflight media experience designed to inspire you. Starting with an exciting section of free content that will get you excited about your destination while you’re still in the air.
         Enjoy blockbuster movies, television, music, and the web with our <a href="http://www.alaskaair.com/content/travel-info/on-board/inflight-entertainment.aspx">inflight entertainment players</a> for as low as $1.99. Or, simply use your own mobile device to access all of the same content and web access with our new inflight app.
         Download it from the app store now and stay connected during your flight.</p>
         <a class="btn btn-green" href="">Learn More</a>
+        <div class="col-md-4 text-center"> 
+<button id="singlebutton" name="singlebutton" class="btn btn-primary">Next Step!</button> 
+</div>
     </div></div>
   </div>
   <div class="container">
     <div class="row">
       <div class="col-sm-12">
         <h3>Locally Sourced Food & Drinks</h3>
-        <img src="http://placehold.it/940x350">
+        <img class="full-screen" src="http://placehold.it/972x350">
         <p>We are always on the hunt for new, tasty local discoveries to bring on board and share with you. Being from the Northwest, we have an amazing community of artisans, chefs, and vineyards to partner with.</p>
         <p>On our flights you will find entrees created by Tom Douglas, a Fruit & Cheese plate featuring Beecher’s handmade cheese, Chateau St. Michelle and Canoe Ridge wines, as well as premium spirts crafted by Sun Liquor Distillery.</p>
         <a class="btn btn-green" href="">Learn More</a>
@@ -44,7 +67,7 @@
     <div class="row">
       <div class="col-sm-12">
         <h3>Power at Every Seat</h3>
-        <img src="http://placehold.it/940x350">
+        <img class="full-screen" src="http://placehold.it/972x350">
         <p>We’ve commissioned custom-designed <a target="_blank" href="http://www.recaro-as.com/home-acs.html">Recaro</a> leather seats, designed to offer you more legroom without compromising comfort or recline.</p>
         <p>Now in every one of those seats, on every Alaska flight, you can stay fully charged with 110 volt power and USB outlets in the seatback. There’s no need to put your life on hold just because you’re at 35,000 feet, stay connected with power and Wi-Fi on every flight.</p>
       </div>
@@ -61,7 +84,7 @@
     </div>
     <div class="container">
       <div class="row">
-      <h2>Lorem Ipsum Title</h2>
+      <div class="col-sm-12"><h2>Lorem Ipsum Title</h2></div>
         <div class="col-sm-4">
           <img src="http://placehold.it/300x150">
           <h3>First Class Experience</h3>
@@ -70,16 +93,42 @@
         <div class="col-sm-4">
           <img src="http://placehold.it/300x150">
           <h3>Flying to Disneyland&reg;</h3>
-          <h4 class="secondary">Beyond Service</h4>
           <p>The magic of your family adventure begins before you land with Disney-themed coloring books, snack packs, and more. Start your Disneyland® trip <a href="http://www.alaskaair.com/content/travel-info/on-board/fly-to-disneyland.aspx">here</a>.</p>
         </div>
         <div class="col-sm-4">
           <img src="http://placehold.it/300x150">
           <h3>Discover Music Onboard</h3>
-          <h4 class="secondary">Beyond Service</h4>
           <p>Being based in Seattle, there are so many things that we share with our hometown. One of them is a deep love of music, check out our inflight music system to discover new, talented West Coast artists. </p>
         </div>
       </div>
     </div>
-  </div>
+</div>
+<script type="text/javascript">
+$(document).ready(function() {
+                
+                                $('#nav').affix({
+  offset: {
+                top: $('#beyondMainImage').height()
+  }
+
+}); 
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+                if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+                  var target = $(this.hash);
+                  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                  if (target.length) {
+                                $('html,body').animate({
+                                  scrollTop: target.offset().top
+                                }, 1000);
+                                return false;
+                  }
+                }
+  });
+});
+
+</script>
+
   <?php include ("../sections/footer.php"); ?>
