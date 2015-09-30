097de1398b93586680dced8bbfdfebf3e938663e
braven112 <braven112@gmail.com>
2015-09-30 09:39:01 -0700

    updating panels with banner

--- pages/panels.php
+++ pages/panels.php
@@ -1,5 +1,42 @@
 <?php include ("../sections/header.php"); ?>
 
-<?php include ("../sections/destinations_1x4.php"); ?>
+<style>
+	.as-picture {
+		overflow: hidden;
+		background-image: url(//resource.alaskaair.net/content/~/media/613A4708A5E44948A3FEE6156567DCCA.ashx);
+		background-size: cover;
+		background-position: top left;
+	}
+	.container-banner {
+		margin-bottom: 24px;
+	}
+	.row-banner {
+		border:1px solid #eee;
+	}
+	.ad-panel .panel-footer {
+		margin: 0 0 0 -15px;
+		padding: 20px 15px 20px 30px;
+
+	}
+	.panel-footer .btn {
+		margin-top: -.5em;
+	}
+
+</style>
+<div class="container container-banner show-if-US hide-if-bofa">
+	<div class="row row-banner">
+		<div class="col-sm-12 col-md-6 as-picture panel-image">
+			
+		</div>
+		<div class="col-sm-12 col-md-6">
+			<div class="ad-panel panel-default">
+				<div class="panel-body">
+					<span class="h3 banner-text">Go more place with 25,000 bonus miles and your first checked bag free.</span>
+				</div>
+				<div id="cc-descritpion" class="panel-footer fine-print">Sign up for the Alaska Airlines Visa Signature&reg; Consumer Card <a class="btn btn-sm btn-ghost-blue pull-right" href="#" aria-describedby="cc-descritpion">Apply Now</a></div>
+			</div>
+		</div>
+	</div>
+</div>
  
 <?php include ("../sections/footer.php"); ?>
