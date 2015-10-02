e10cac10e144fcd7eb9431ebf2060ae0d457cc27
braven112 <braven112@gmail.com>
2015-10-02 11:02:18 -0700

    jquery

--- pages/banners.php
+++ pages/banners.php
@@ -1,5 +1,5 @@
 <?php include ("../sections/header.php"); ?>
-<div class="container container-banner show-if-US hide-if-bofa">
+<div id="banner-1" class="container container-banner show-if-US hide-if-bofa">
 	<a href="#">
 		<div class="row row-banner">
 			<div class="col-sm-12 col-md-5 bank-picture panel-image">
@@ -20,27 +20,8 @@
 <div class="container">
 	<div class="row">
 		<div class="col-sm-12">
-			<h3>Optional Classes</h3>
-			<p>Include classes like <code>show-if-US</code> <code>show-if-ca</code> <code>show-if-bofa</code> <code>hide-if-bofa</code>when you need to create multinational ads and need to restrict an offer to certain groups</p>
-			<textarea class="embed-responsive embed-responsive-16by9">
-			<div class="container container-banner show-if-US hide-if-bofa">
-				<a href="#">
-					<div class="row row-banner">
-						<div class="col-sm-12 col-md-5 bank-picture panel-image">
-							
-						</div>
-						<div class="col-sm-12 col-md-7">
-							<div class="ad-panel panel-default">
-								<div class="panel-body">
-									<div class="h3 banner-text">Go more place with 25,000 bonus miles and your first checked bag free.</div>
-								</div>
-								<div id="cc-descritpion" class="panel-footer fine-print">Sign up for the Alaska Airlines Visa Signature® Consumer Card <button type="button" class="btn btn-sm btn-ghost-blue pull-right hidden-xs">Apply Now</button>
-								<button type="button" class="btn btn-ghost-blue visible-xs">Apply Now</button></div>
-							</div>
-						</div>
-					</div>
-				</a>
-			</div>
+			<textarea class="form-control" rows="5" id="banner-code-1">
+			
 			</textarea>
 						<h3>Optional Classes</h3>
 			<p>Include classes like <code>show-if-US</code> <code>show-if-ca</code> <code>show-if-bofa</code> <code>hide-if-bofa</code>when you need to create multinational ads and need to restrict an offer to certain groups</p>
@@ -48,4 +29,10 @@
 		</div>
 	</div>
 </div>
+
+<script type="text/javascript">
+	
+$('#banner-code-1').append('#banner-1');
+
+</script>
 <?php include ("../sections/footer.php"); ?>
