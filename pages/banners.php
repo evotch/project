fb2778f33d640f1beab9552938fe311e66d1686d
braven112 <braven112@gmail.com>
2015-10-02 11:33:56 -0700

    asdf

--- pages/banners.php
+++ pages/banners.php
@@ -1,5 +1,27 @@
 <?php include ("../sections/header.php"); ?>
-<div id="banner-1" class="container container-banner show-if-US hide-if-bofa">
+<style type="text/css">
+@font-face {
+	font-family: Circular;
+	src: url('https://sp-bootstrap.global.ssl.fastly.net/5.2.0/fonts/circular-book.woff2') format('woff2'), url('https://sp-bootstrap.global.ssl.fastly.net/5.2.0/fonts/circular-light.woff') format('woff');
+	font-weight: normal;
+	font-style: normal
+}
+@font-face {
+	font-family: Circular;
+	src: url('https://sp-bootstrap.global.ssl.fastly.net/5.2.0/fonts/circular-bold.woff2') format('woff2'), url('https://sp-bootstrap.global.ssl.fastly.net/5.2.0/fonts/circular-bold.woff') format('woff');
+	font-weight: bold;
+}
+</style>
+
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			<h2>Banner #1</h2>
+		</div>
+	</div>
+</div>
+
+<div id="banner-1" class="container container-banner">
 	<a href="#">
 		<div class="row row-banner">
 			<div class="col-sm-12 col-md-5 bank-picture panel-image">
@@ -21,7 +43,24 @@
 	<div class="row">
 		<div class="col-sm-12">
 			<textarea class="form-control" rows="5" id="banner-code-1">
-			
+<div id="banner-1" class="container container-banner">
+	<a href="#">
+		<div class="row row-banner">
+			<div class="col-sm-12 col-md-5 bank-picture panel-image">
+				
+			</div>
+			<div class="col-sm-12 col-md-7">
+				<div class="ad-panel panel-default">
+					<div class="panel-body">
+						<div class="h3 banner-text">Go more place with 25,000 bonus miles and your first checked bag free.</div>
+					</div>
+					<div id="cc-descritpion" class="panel-footer fine-print">Sign up for the Alaska Airlines Visa Signature® Consumer Card <button type="button" class="btn btn-sm btn-ghost-blue pull-right hidden-xs">Apply Now</button>
+					<button type="button" class="btn btn-ghost-blue visible-xs">Apply Now</button></div>
+				</div>
+			</div>
+		</div>
+	</a>
+</div>
 			</textarea>
 						<h3>Optional Classes</h3>
 			<p>Include classes like <code>show-if-US</code> <code>show-if-ca</code> <code>show-if-bofa</code> <code>hide-if-bofa</code>when you need to create multinational ads and need to restrict an offer to certain groups</p>
@@ -30,11 +69,5 @@
 	</div>
 </div>
 
-<script type="text/javascript">
 
-
-$( "#banner-code-1" ).append( $( "#banner-1" ) );
-	
-
-</script>
 <?php include ("../sections/footer.php"); ?>
