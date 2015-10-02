851a422b6e1f56621c03ae374321aa8f512f666d
braven112 <braven112@gmail.com>
2015-10-02 10:49:34 -0700

    updates

--- pages/banners.php
+++ pages/banners.php
@@ -1,44 +1,51 @@
 <?php include ("../sections/header.php"); ?>
-
-
 <div class="container container-banner show-if-US hide-if-bofa">
 	<a href="#">
-	<div class="row row-banner">
-		<div class="col-sm-12 col-md-5 bank-picture panel-image">
-			
-		</div>
-		<div class="col-sm-12 col-md-7">
-			<div class="ad-panel panel-default">
-				<div class="panel-body">
-					<div class="h3 banner-text">Go more place with 25,000 bonus miles and your first checked bag free.</div>
+		<div class="row row-banner">
+			<div class="col-sm-12 col-md-5 bank-picture panel-image">
+				
+			</div>
+			<div class="col-sm-12 col-md-7">
+				<div class="ad-panel panel-default">
+					<div class="panel-body">
+						<div class="h3 banner-text">Go more place with 25,000 bonus miles and your first checked bag free.</div>
+					</div>
+					<div id="cc-descritpion" class="panel-footer fine-print">Sign up for the Alaska Airlines Visa Signature® Consumer Card <button type="button" class="btn btn-sm btn-ghost-blue pull-right hidden-xs">Apply Now</button>
+					<button type="button" class="btn btn-ghost-blue visible-xs">Apply Now</button></div>
 				</div>
-				<div id="cc-descritpion" class="panel-footer fine-print">Sign up for the Alaska Airlines Visa Signature® Consumer Card <button type="button" class="btn btn-sm btn-ghost-blue pull-right hidden-xs">Apply Now</button>
-				<button type="button" class="btn btn-ghost-blue visible-xs">Apply Now</button></div>
 			</div>
 		</div>
-	</div>
 	</a>
 </div>
-<p>The code below includes classes <code>show-if-US</code> <code>hide-if-bofa</code> use these when you need to create multinational ads and need to restrict an offer to the U.S. only.</p>
-<textarea>
-<div class="container container-banner show-if-US hide-if-bofa">
-	<a href="#">
-	<div class="row row-banner">
-		<div class="col-sm-12 col-md-5 bank-picture panel-image">
-			
-		</div>
-		<div class="col-sm-12 col-md-7">
-			<div class="ad-panel panel-default">
-				<div class="panel-body">
-					<div class="h3 banner-text">Go more place with 25,000 bonus miles and your first checked bag free.</div>
-				</div>
-				<div id="cc-descritpion" class="panel-footer fine-print">Sign up for the Alaska Airlines Visa Signature® Consumer Card <button type="button" class="btn btn-sm btn-ghost-blue pull-right hidden-xs">Apply Now</button>
-				<button type="button" class="btn btn-ghost-blue visible-xs">Apply Now</button></div>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			<h3>Optional Classes</h3>
+			<p>Include classes like <code>show-if-US</code> <code>show-if-ca</code> <code>show-if-bofa</code> <code>hide-if-bofa</code>when you need to create multinational ads and need to restrict an offer to certain groups</p>
+			<textarea>
+			<div class="container container-banner show-if-US hide-if-bofa">
+				<a href="#">
+					<div class="row row-banner">
+						<div class="col-sm-12 col-md-5 bank-picture panel-image">
+							
+						</div>
+						<div class="col-sm-12 col-md-7">
+							<div class="ad-panel panel-default">
+								<div class="panel-body">
+									<div class="h3 banner-text">Go more place with 25,000 bonus miles and your first checked bag free.</div>
+								</div>
+								<div id="cc-descritpion" class="panel-footer fine-print">Sign up for the Alaska Airlines Visa Signature® Consumer Card <button type="button" class="btn btn-sm btn-ghost-blue pull-right hidden-xs">Apply Now</button>
+								<button type="button" class="btn btn-ghost-blue visible-xs">Apply Now</button></div>
+							</div>
+						</div>
+					</div>
+				</a>
 			</div>
+			</textarea>
+						<h3>Optional Classes</h3>
+			<p>Include classes like <code>show-if-US</code> <code>show-if-ca</code> <code>show-if-bofa</code> <code>hide-if-bofa</code>when you need to create multinational ads and need to restrict an offer to certain groups</p>
+
 		</div>
 	</div>
-	</a>
 </div>
-</textarea>
- 
 <?php include ("../sections/footer.php"); ?>
