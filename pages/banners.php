7a6363610d8686ae82ef52b8edd0e8cb32c5078e
braven112 <braven112@gmail.com>
2015-10-09 15:24:47 -0700

    lots of new stuff

--- pages/banners.php
+++ pages/banners.php
@@ -1,73 +1,283 @@
 <?php include ("../sections/header.php"); ?>
-<style type="text/css">
-@font-face {
-	font-family: Circular;
-	src: url('https://sp-bootstrap.global.ssl.fastly.net/5.2.0/fonts/circular-book.woff2') format('woff2'), url('https://sp-bootstrap.global.ssl.fastly.net/5.2.0/fonts/circular-light.woff') format('woff');
-	font-weight: normal;
-	font-style: normal
-}
-@font-face {
-	font-family: Circular;
-	src: url('https://sp-bootstrap.global.ssl.fastly.net/5.2.0/fonts/circular-bold.woff2') format('woff2'), url('https://sp-bootstrap.global.ssl.fastly.net/5.2.0/fonts/circular-bold.woff') format('woff');
-	font-weight: bold;
-}
-</style>
+  <style type="text/css">
+  @font-face {
+  font-family: Circular;
+  src: url('https://sp-bootstrap.global.ssl.fastly.net/5.2.0/fonts/circular-book.woff2') format('woff2'), url('https://sp-bootstrap.global.ssl.fastly.net/5.2.0/fonts/circular-light.woff') format('woff');
+  font-weight: normal;
+  font-style: normal
+  }
+  @font-face {
+  font-family: Circular;
+  src: url('https://sp-bootstrap.global.ssl.fastly.net/5.2.0/fonts/circular-bold.woff2') format('woff2'), url('https://sp-bootstrap.global.ssl.fastly.net/5.2.0/fonts/circular-bold.woff') format('woff');
+  font-weight: bold;
+  }
+  body {font-family:Circular, Arial !important}
+  .pure-menu, .pure-g, .pure-g [class*="pure-u"] {
+  font-family: Circular;
+  }
+  textarea.form-control {
+  margin-bottom: 24px;
+  font-size: 11px;
+  line-height: 1;
+  color: #666666;
+  background-color: #FBFBFB;
+  border: 1px solid #ddd;
+  }
+  .hf.hf-f {
+  margin-top: 48px
+  }
+  section {
+  margin-bottom: 24px;
+  }
+  </style>
 
-<div class="container">
-	<div class="row">
-		<div class="col-sm-12">
-			<h2>Banner #1</h2>
-		</div>
-	</div>
-</div>
+    <section id="intro-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="page-header">
+              <h2>Welcome to Ad Creator.</h2>
+              <h4 class="gray secondary">Even you can do it.<sup>&trade;</sup></h4>
+            </div>
+            <p>Each ad is designed to meet accesibility guidelines and work on all screen sizes.  Each utilizes Bootstrap's grid system for easy sizing, as well as panel classes to change background colors.  To change the background colors include or change the code to <code>panel-navy</code> <code>panel-white</code></p>
+            <p>A library of background images along with their corresponding css class to customize these banners is also available. <a href="#">Working link to come</a></p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="banner-1-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <h2 class="section-header">Banner #1</h2>
+          </div>
+        </div>
+      </div>
+      
+      <div id="banner-1" class="container container-banner">
+        <a href="#">
+          <div class="row row-banner">
+            <div aria-label="Image of Puertovallarta" class="col-sm-3 col-md-5 destination-puertovallarta2 panel-image">
+              
+            </div>
+            <div class="col-sm-9 col-md-7" contenteditable="true">
+              <div class="xsell-panel panel-default">
+                <div class="panel-body">
+                  <span class="h3 banner-text">Go more place with 25,000 bonus miles and your first checked bag free.</span>
+                  <div class="sub-text">How about leaving today?</div>
+                </div>
+                <div id="cc-descritpion" class="panel-footer">Sign up for the Alaska Airlines Visa Signature® Consumer Card <button type="button" class="btn btn-sm btn-ghost-blue pull-right hidden-xs" contenteditable="true">Apply Now</button>
+                <button type="button" class="btn btn-ghost-blue visible-xs" contenteditable="true">Apply Now</button></div>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+      
+      <div id="display-code4banner1" class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <textarea class="form-control" rows="5" id="banner-code-1">
+            <div id="banner-1" class="container container-banner">
+              <a href="#">
+                <div class="row row-banner">
+                  <div aria-label="Image of Puertovallarta" class="col-sm-3 col-md-5 destination-puertovallarta2 panel-image">
+                    
+                  </div>
+                  <div class="col-sm-9 col-md-7">
+                    <div class="xsell-panel panel-default">
+                      <div class="panel-body">
+                        <span class="h3 banner-text">Go more place with 25,000 bonus miles and your first checked bag free.</span>
+                        <div class="sub-text">How about leaving today?</div>
+                      </div>
+                      <div id="cc-descritpion" class="panel-footer">Sign up for the Alaska Airlines Visa Signature® Consumer Card <button type="button" class="btn btn-sm btn-ghost-blue pull-right hidden-xs" contenteditable="true">Apply Now</button>
+                      <button type="button" class="btn btn-ghost-blue visible-xs">Apply Now</button></div>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+            </textarea>
+            <!-- <h5>Notes</h5>
+            <p>Include classes like <code>show-if-US</code> <code>show-if-ca</code> <code>show-if-bofa</code> <code>hide-if-bofa</code>when you need to create multinational ads and need to restrict an offer to certain groups</p> -->
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="banner-2-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <h2 class="section-header">Banner #2</h2>
+          </div>
+        </div>
+      </div>
+      
+      <div id="banner-2" class="container container-banner">
+        <a href="#">
+          <div class="row row-banner">
+            <div aria-label="Image of couple sitting on a moped" class="col-sm-push-9 col-sm-3 col-md-push-7 col-md-5 feature-moped panel-image">
+              
+            </div>
+            <div class="col-sm-pull-3 col-sm-9 col-md-pull-5 col-md-7 xsell-panel panel-navy" contenteditable="true">
+              <div class="panel-body">
+                <span class="h3 banner-text">Go more place with 25,000 bonus miles and your first checked bag free.</span>
+                <div class="banner-text">How about leaving today?</div>
+              </div>
+              <div id="cc-descritpion" class="panel-footer panel-navy">Sign up for the Alaska Airlines Visa Signature® Consumer Card <button type="button" class="btn btn-sm btn-white pull-right hidden-xs">Apply Now</button>
+                <button type="button" class="btn btn-white visible-xs">Apply Now</button>
+              </div>
+            </div>
+            
+          </div>
+        </a>
+      </div>
+      <div id="display-code4banner2" class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <textarea class="form-control" rows="5" id="banner-code-1">
+            <div class="container container-banner">
+              <a href="#">
+                <div class="row row-banner">
+                  <div aria-label="Image of couple sitting on a moped" class="col-sm-push-9 col-sm-3 col-md-push-7 col-md-5 feature-moped panel-image">
+                    
+                  </div>
+                  <div class="col-sm-pull-3 col-sm-9 col-md-pull-5 col-md-7 xsell-panel panel-navy" contenteditable="true">
+                    <div class="panel-body">
+                      <span class="h3 banner-text">Go more place with 25,000 bonus miles and your first checked bag free.</span>
+                      <div class="banner-text">How about leaving today?</div>
+                    </div>
+                    <div id="cc-descritpion" class="panel-footer panel-navy">Sign up for the Alaska Airlines Visa Signature® Consumer Card <button type="button" class="btn btn-sm btn-white pull-right hidden-xs">Apply Now</button>
+                      <button type="button" class="btn btn-white visible-xs">Apply Now</button>
+                    </div>
+                  </div>
+                  
+                </div>
+              </a>
+            </div>
+            </textarea>
+            <!-- <h5>Optional Classes</h5>
+            <p>Include classes like <code>show-if-US</code> <code>show-if-ca</code> <code>show-if-bofa</code> <code>hide-if-bofa</code>when you need to create multinational ads and need to restrict an offer to certain groups</p> -->
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="banner-3-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <h2 class="section-header">Banner #3</h2>
+          </div>
+        </div>
+      </div>
+      
+      <div id="banner-3" class="container container-banner">
+        <a href="#">
+          <div class="row row-banner">
+            <div aria-label="Image of Alaska" class="col-sm-4 col-md-6 temp-1 panel-image">
+              
+            </div>
+            <div class="col-sm-8 col-md-6 xsell-panel panel-navy" contenteditable="true">
+              <div class="panel-body panel-body-1color">
+                <div class="h3 banner-text">See more destinations and earn more rewards with Alaska Airlines and our global partners.</div>
+                
+              <button contenteditable="true" type="button" class="btn btn-white">Learn More</button></div>
+              
+            </div>
+            
+          </div>
+        </a>
+      </div>
+      <div id="display-code4banner3" class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <textarea class="form-control" rows="5" id="banner-code-3">
+            <div class="container container-banner">
+              <a href="#">
+                <div class="row row-banner">
+                  <div aria-label="Image of Alaska" class="col-sm-4 col-md-6 temp-1 panel-image">
+                    
+                  </div>
+                  <div class="col-sm-8 col-md-6 xsell-panel panel-navy" contenteditable="true">
+                    <div class="panel-body panel-body-1color">
+                      <div class="h3 banner-text">Alaska Airlines and Acme Air partner.  It's time to go.  Beep. Beep.</div>
+                      
+                    <button contenteditable="true" type="button" class="btn btn-white">Learn More</button></div>
+                    
+                  </div>
+                  
+                </div>
+              </a>
+            </div>
+            </textarea>
+            <!-- <h5>Optional Classes</h5>
+            <p>Include classes like <code>show-if-US</code> <code>show-if-ca</code> <code>show-if-bofa</code> <code>hide-if-bofa</code>when you need to create multinational ads and need to restrict an offer to certain groups</p> -->
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="banner-xsell-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <h2 class="section-header">Cross Sell Banners</h2>
+          </div>
+        </div>
+      </div>
+      
+      <div id="banner-xsell" class="container">
+              
+            <div class="row">
+              
+              <div class="col-sm-8">
+                <div class="container-banner">
+                  <a href="#">
+                    <div class="row row-banner">
+                      <div class="col-sm-12 xsell-panel panel-navy" contenteditable="true">
+                        <div class="panel-body">
+                          <span class="h2 banner-text">Go more place with 25,000 Miles and your first checked bag free.</span>
+                          <div class="banner-text">Sign up now for &nbsp;the Alaska Airlines&nbsp;Visa Signature Consumer Card.</div>
+                        </div>
+                        <div id="cc-descritpion" class="panel-footer panel-navy">&nbsp;Offer ends soon!<button type="button" class="btn btn-sm btn-white pull-right hidden-xs">Apply Now</button>
+                          <button type="button" class="btn btn-white visible-xs">Apply Now</button>
+                        </div>
+                      </div>
+                      
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
 
-<div id="banner-1" class="container container-banner">
-	<a href="#">
-		<div class="row row-banner">
-			<div class="col-sm-12 col-md-5 bank-picture panel-image">
-				
-			</div>
-			<div class="col-sm-12 col-md-7">
-				<div class="ad-panel panel-default">
-					<div class="panel-body">
-						<div class="h3 banner-text">Go more place with 25,000 bonus miles and your first checked bag free.</div>
-					</div>
-					<div id="cc-descritpion" class="panel-footer fine-print">Sign up for the Alaska Airlines Visa Signature® Consumer Card <button type="button" class="btn btn-sm btn-ghost-blue pull-right hidden-xs">Apply Now</button>
-					<button type="button" class="btn btn-ghost-blue visible-xs">Apply Now</button></div>
-				</div>
-			</div>
-		</div>
-	</a>
-</div>
-<div class="container">
-	<div class="row">
-		<div class="col-sm-12">
-			<textarea class="form-control" rows="5" id="banner-code-1">
-<div id="banner-1" class="container container-banner">
-	<a href="#">
-		<div class="row row-banner">
-			<div class="col-sm-12 col-md-5 bank-picture panel-image">
-				
-			</div>
-			<div class="col-sm-12 col-md-7">
-				<div class="ad-panel panel-default">
-					<div class="panel-body">
-						<div class="h3 banner-text">Go more place with 25,000 bonus miles and your first checked bag free.</div>
-					</div>
-					<div id="cc-descritpion" class="panel-footer fine-print">Sign up for the Alaska Airlines Visa Signature® Consumer Card <button type="button" class="btn btn-sm btn-ghost-blue pull-right hidden-xs">Apply Now</button>
-					<button type="button" class="btn btn-ghost-blue visible-xs">Apply Now</button></div>
-				</div>
-			</div>
-		</div>
-	</a>
-</div>
-			</textarea>
-						<h3>Optional Classes</h3>
-			<p>Include classes like <code>show-if-US</code> <code>show-if-ca</code> <code>show-if-bofa</code> <code>hide-if-bofa</code>when you need to create multinational ads and need to restrict an offer to certain groups</p>
-
-		</div>
-	</div>
-</div>
+      </div>
+      <div id="display-code4xsell" class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <textarea class="form-control" rows="5" id="banner-code-3">
+            <div class="container-banner">
+                  <a href="#">
+                    <div class="row row-banner">
+                      <div class="col-sm-12 xsell-panel panel-navy" contenteditable="true">
+                        <div class="panel-body">
+                          <span class="h2 banner-text">Go more place with 25,000 Miles and your first checked bag free.</span>
+                          <div class="banner-text">Sign up now for &nbsp;the Alaska Airlines&nbsp;Visa Signature Consumer Card.</div>
+                        </div>
+                        <div id="cc-descritpion" class="panel-footer panel-navy">&nbsp;Offer ends soon!<button type="button" class="btn btn-sm btn-white pull-right hidden-xs">Apply Now</button>
+                          <button type="button" class="btn btn-white visible-xs">Apply Now</button>
+                        </div>
+                      </div>
+                      
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </textarea>
+            <!-- <h5>Optional Classes</h5>
+            <p>Include classes like <code>show-if-US</code> <code>show-if-ca</code> <code>show-if-bofa</code> <code>hide-if-bofa</code>when you need to create multinational ads and need to restrict an offer to certain groups</p>
+          </div> -->
+        </div>
+      </div>
+    </section>
 
 
 <?php include ("../sections/footer.php"); ?>
